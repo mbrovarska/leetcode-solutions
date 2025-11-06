d8d687a03fce74a147104884ce34356985d6edc5
mbrovarska <marybrovarska@gmail.com>
2025-11-06 11:53:51 -0500

    1-GO, 1,26,88,125-JS

---
+++ GO/1.go
@@ -0,0 +1,27 @@
+package main
+
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+*/
+
+func twoSum(nums []int, target int) []int {
+    m:= make(map[int]int)
+
+    for i:= 0; i < len(nums); i++ {
+        curr := nums[i]
+        x := target - curr
+        value, ok := m[x]
+        if ok {
+            return []int{value, i}
+        } else {
+            m[curr] = i
+        }
+
+    }
+    return nil
+    
+}
